ebdbf1d2134a27c01a78ece6a88d88bd5ee712dd
Chris Murphy <_chrism_@live.com>
2018-02-16 23:46:32 +0000

    began set-up for album class

--- models/album.rb
+++ models/album.rb
@@ -1,1 +1,14 @@
 require_relative( '../db/sql_runner' )
+
+class Album
+  attr_accessor :title, :quantity
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @title = options['title']
+    @quantity = options['quantity'].to_i
+
+  end
+
+end
